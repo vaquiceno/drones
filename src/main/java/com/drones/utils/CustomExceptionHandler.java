08bd55448a0395dfa2f0f96aaca052fd1bb4ed83
vaquiceno <victoralbertoq@yahoo.com.co>
2023-04-27 03:56:42 -0500

    create custom ExceptionHandler to retrieve a readable http bad request response when Request objects are not passing validations

---
+++ src/main/java/com/drones/utils/CustomExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.drones.utils;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
