fbf877bef3ec9f654ba637227f689bda5723215e
vaquiceno <victoralbertoq@yahoo.com.co>
2023-05-02 03:12:58 -0500

    add Lombok to DroneGeneralException

--- src/main/java/com/drones/models/exceptions/DroneGeneralException.java
+++ src/main/java/com/drones/models/exceptions/DroneGeneralException.java
@@ -1,5 +1,10 @@
 package com.drones.models.exceptions;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
 public class DroneGeneralException extends Exception{
     public DroneGeneralException(String message) {
         super(message);
