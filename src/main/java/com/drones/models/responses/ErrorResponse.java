3b0a97bdd1640b82571294bdaf46d3ae028be005
vaquiceno <victoralbertoq@yahoo.com.co>
2023-05-04 13:58:50 -0500

    add empty constructor to ErrorResponse

--- src/main/java/com/drones/models/responses/ErrorResponse.java
+++ src/main/java/com/drones/models/responses/ErrorResponse.java
@@ -2,8 +2,10 @@ package com.drones.models.responses;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class ErrorResponse {
     private String error;
