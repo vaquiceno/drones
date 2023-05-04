646f93d9dd3aa7181595ccb6f9c64495bc63a2e6
vaquiceno <victoralbertoq@yahoo.com.co>
2023-05-04 14:00:48 -0500

    finish DroneControllerTest

--- src/test/java/com/drones/integration/controllers/DroneControllerTest.java
+++ src/test/java/com/drones/integration/controllers/DroneControllerTest.java
@@ -4,11 +4,16 @@ package com.drones.integration.controllers;
 import com.drones.DronesApplication;
 import com.drones.Mocks;
 import com.drones.mappers.DroneMapper;
+import com.drones.models.database.Drone;
+import com.drones.models.database.DroneLoad;
+import com.drones.models.database.DroneLoadMedication;
+import com.drones.models.database.Medication;
 import com.drones.models.requests.DroneLoadMedicationsRequest;
 import com.drones.models.requests.MedicationRequest;
 import com.drones.models.responses.DroneLoadMedicationResponse;
 import com.drones.models.responses.DroneLoadResponse;
 import com.drones.models.responses.DroneResponse;
+import com.drones.models.responses.ErrorResponse;
 import com.drones.repositories.DroneLoadRepository;
 import com.drones.repositories.DroneRepository;
 import com.drones.repositories.MedicationRepository;
@@ -20,16 +25,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.drones.models.database.Drone.Model.Lightweight;
 import static com.drones.models.database.Drone.Model.Middleweight;
+import static com.drones.models.database.Drone.Status.DELIVERED;
+import static com.drones.models.database.Drone.Status.DELIVERING;
 import static com.drones.models.database.Drone.Status.IDLE;
+import static com.drones.models.database.Drone.Status.LOADED;
 import static com.drones.models.database.Drone.Status.LOADING;
+import static com.drones.models.database.Drone.Status.RETURNING;
+import static com.drones.utils.Constants.ERROR_MESSAGE_ZERO_MORE_ONE_ACTIVE_LOADS;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,7 +49,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @AutoConfigureMockMvc
 @SpringBootTest(classes = DronesApplication.class)
-@Transactional
+@Transactional()
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class DroneControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -64,8 +77,8 @@ class DroneControllerTest {
     @Test
     void getAllDrones() throws Exception {
         String result = mockMvc.perform(
-                MockMvcRequestBuilders
-                        .get(ENDPOINT_BASE+"/all"))
+                        MockMvcRequestBuilders
+                                .get(ENDPOINT_BASE+"/all"))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
@@ -106,6 +119,22 @@ class DroneControllerTest {
         assertEquals(
                 droneResponse,
                 mocks.baseDroneResponse(1, "sn1", Lightweight.toString(), 500, 100, IDLE.toString())
+        );
+    }
+
+    @Test
+    void getLoadedMedications() throws Exception {
+        String result = mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get(ENDPOINT_BASE+"/loaded_medications/"+mocks.DRONE_ID))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        ErrorResponse droneLoadResponse = objectMapper.readValue(result, ErrorResponse.class);
+        assertEquals(
+                droneLoadResponse,
+                new ErrorResponse(ERROR_MESSAGE_ZERO_MORE_ONE_ACTIVE_LOADS)
         );
     }
 
@@ -179,6 +208,273 @@ class DroneControllerTest {
         assertEquals(
                 droneLoadResponse,
                 mocks.baseDroneLoadResponse(LOADING, droneLoadMedicationResponses)
+        );
+    }
+
+    @Test
+    void loadedDrone() throws Exception {
+        Medication medication = mocks.baseMedication(
+                "6985",
+                "Bread",
+                10,
+                "https://regex101.com//img.das");
+        medicationRepository.save(medication);
+        Drone drone = droneRepository.save(mocks.baseDroneIdNull);
+        DroneLoad droneLoad = droneMapper.toDroneLoad(drone, LocalDateTime.now());
+        droneLoad = droneLoadRepository.save(droneLoad);
+        // save Drone Load Medication
+        DroneLoad finalDroneLoad = droneLoad;
+        finalDroneLoad.addDroneLoadMedication(
+                DroneLoadMedication
+                        .builder()
+                        .droneLoad(finalDroneLoad)
+                        .medication(medication)
+                        .amount(2)
+                        .build()
+        );
+        // set drone Status to LOADING
+        drone.setStatus(LOADING);
+        droneLoadRepository.save(finalDroneLoad);
+        droneRepository.save(drone);
+        Integer droneId = drone.getId();
+        List<DroneLoadMedicationResponse> droneLoadMedicationResponses = List.of(
+                DroneLoadMedicationResponse
+                        .builder()
+                        .code("6985")
+                        .name("Bread")
+                        .weight(10)
+                        .imageUrl("https://regex101.com//img.das")
+                        .amount(2)
+                        .build()
+        );
+
+        String result = mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .put(ENDPOINT_BASE+"/loaded/"+droneId))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        DroneLoadResponse droneLoadResponse = objectMapper.readValue(result, DroneLoadResponse.class);
+        assertNotNull(droneLoadResponse.getStartTime());
+        droneLoadResponse.setStartTime(mocks.DRONE_LOAD_START_TIME);
+        assertEquals(
+                droneLoadResponse,
+                mocks.baseDroneLoadResponse(droneId, LOADED, droneLoadMedicationResponses)
+        );
+    }
+
+    @Test
+    void deliveringDrone() throws Exception{
+        Medication medication = mocks.baseMedication(
+                "6985",
+                "Bread",
+                10,
+                "https://regex101.com//img.das");
+        medicationRepository.save(medication);
+        Drone drone = droneRepository.save(mocks.baseDroneIdNull);
+        DroneLoad droneLoad = droneMapper.toDroneLoad(drone, LocalDateTime.now());
+        droneLoad = droneLoadRepository.save(droneLoad);
+        // save Drone Load Medication
+        DroneLoad finalDroneLoad = droneLoad;
+        finalDroneLoad.addDroneLoadMedication(
+                DroneLoadMedication
+                        .builder()
+                        .droneLoad(finalDroneLoad)
+                        .medication(medication)
+                        .amount(2)
+                        .build()
+        );
+        // set drone Status to LOADED
+        drone.setStatus(LOADED);
+        droneLoadRepository.save(finalDroneLoad);
+        droneRepository.save(drone);
+        Integer droneId = drone.getId();
+        List<DroneLoadMedicationResponse> droneLoadMedicationResponses = List.of(
+                DroneLoadMedicationResponse
+                        .builder()
+                        .code("6985")
+                        .name("Bread")
+                        .weight(10)
+                        .imageUrl("https://regex101.com//img.das")
+                        .amount(2)
+                        .build()
+        );
+
+        String result = mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .put(ENDPOINT_BASE+"/delivering/"+droneId))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        DroneLoadResponse droneLoadResponse = objectMapper.readValue(result, DroneLoadResponse.class);
+        assertNotNull(droneLoadResponse.getStartTime());
+        droneLoadResponse.setStartTime(mocks.DRONE_LOAD_START_TIME);
+        assertEquals(
+                droneLoadResponse,
+                mocks.baseDroneLoadResponse(droneId, DELIVERING, droneLoadMedicationResponses)
+        );
+    }
+
+    @Test
+    void deliveredDrone() throws Exception{
+        Medication medication = mocks.baseMedication(
+                "6985",
+                "Bread",
+                10,
+                "https://regex101.com//img.das");
+        medicationRepository.save(medication);
+        Drone drone = droneRepository.save(mocks.baseDroneIdNull);
+        DroneLoad droneLoad = droneMapper.toDroneLoad(drone, LocalDateTime.now());
+        droneLoad = droneLoadRepository.save(droneLoad);
+        // save Drone Load Medication
+        DroneLoad finalDroneLoad = droneLoad;
+        finalDroneLoad.addDroneLoadMedication(
+                DroneLoadMedication
+                        .builder()
+                        .droneLoad(finalDroneLoad)
+                        .medication(medication)
+                        .amount(2)
+                        .build()
+        );
+        // set drone Status to DELIVERING
+        drone.setStatus(DELIVERING);
+        droneLoadRepository.save(finalDroneLoad);
+        droneRepository.save(drone);
+        Integer droneId = drone.getId();
+        List<DroneLoadMedicationResponse> droneLoadMedicationResponses = List.of(
+                DroneLoadMedicationResponse
+                        .builder()
+                        .code("6985")
+                        .name("Bread")
+                        .weight(10)
+                        .imageUrl("https://regex101.com//img.das")
+                        .amount(2)
+                        .build()
+        );
+
+        String result = mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .put(ENDPOINT_BASE+"/delivered/"+droneId))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        DroneLoadResponse droneLoadResponse = objectMapper.readValue(result, DroneLoadResponse.class);
+        assertNotNull(droneLoadResponse.getStartTime());
+        droneLoadResponse.setStartTime(mocks.DRONE_LOAD_START_TIME);
+        assertEquals(
+                droneLoadResponse,
+                mocks.baseDroneLoadResponse(droneId, DELIVERED, droneLoadMedicationResponses)
+        );
+    }
+
+    @Test
+    void returningDrone() throws Exception{
+        Medication medication = mocks.baseMedication(
+                "6985",
+                "Bread",
+                10,
+                "https://regex101.com//img.das");
+        medicationRepository.save(medication);
+        Drone drone = droneRepository.save(mocks.baseDroneIdNull);
+        DroneLoad droneLoad = droneMapper.toDroneLoad(drone, LocalDateTime.now());
+        droneLoad = droneLoadRepository.save(droneLoad);
+        // save Drone Load Medication
+        DroneLoad finalDroneLoad = droneLoad;
+        finalDroneLoad.addDroneLoadMedication(
+                DroneLoadMedication
+                        .builder()
+                        .droneLoad(finalDroneLoad)
+                        .medication(medication)
+                        .amount(2)
+                        .build()
+        );
+        // set drone Status to DELIVERED
+        drone.setStatus(DELIVERED);
+        droneLoadRepository.save(finalDroneLoad);
+        droneRepository.save(drone);
+        Integer droneId = drone.getId();
+        List<DroneLoadMedicationResponse> droneLoadMedicationResponses = List.of(
+                DroneLoadMedicationResponse
+                        .builder()
+                        .code("6985")
+                        .name("Bread")
+                        .weight(10)
+                        .imageUrl("https://regex101.com//img.das")
+                        .amount(2)
+                        .build()
+        );
+
+        String result = mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .put(ENDPOINT_BASE+"/returning/"+droneId))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        DroneLoadResponse droneLoadResponse = objectMapper.readValue(result, DroneLoadResponse.class);
+        assertNotNull(droneLoadResponse.getStartTime());
+        droneLoadResponse.setStartTime(mocks.DRONE_LOAD_START_TIME);
+        assertEquals(
+                droneLoadResponse,
+                mocks.baseDroneLoadResponse(droneId, RETURNING, droneLoadMedicationResponses)
+        );
+    }
+
+    @Test
+    void idleDrone() throws Exception{
+        Medication medication = mocks.baseMedication(
+                "6985",
+                "Bread",
+                10,
+                "https://regex101.com//img.das");
+        medicationRepository.save(medication);
+        Drone drone = droneRepository.save(mocks.baseDroneIdNull);
+        DroneLoad droneLoad = droneMapper.toDroneLoad(drone, LocalDateTime.now());
+        droneLoad = droneLoadRepository.save(droneLoad);
+        // save Drone Load Medication
+        DroneLoad finalDroneLoad = droneLoad;
+        finalDroneLoad.addDroneLoadMedication(
+                DroneLoadMedication
+                        .builder()
+                        .droneLoad(finalDroneLoad)
+                        .medication(medication)
+                        .amount(2)
+                        .build()
+        );
+        // set drone Status to RETURNING
+        drone.setStatus(RETURNING);
+        droneLoadRepository.save(finalDroneLoad);
+        droneRepository.save(drone);
+        Integer droneId = drone.getId();
+        List<DroneLoadMedicationResponse> droneLoadMedicationResponses = List.of(
+                DroneLoadMedicationResponse
+                        .builder()
+                        .code("6985")
+                        .name("Bread")
+                        .weight(10)
+                        .imageUrl("https://regex101.com//img.das")
+                        .amount(2)
+                        .build()
+        );
+
+        String result = mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .put(ENDPOINT_BASE+"/idle/"+droneId))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        DroneLoadResponse droneLoadResponse = objectMapper.readValue(result, DroneLoadResponse.class);
+        assertNotNull(droneLoadResponse.getStartTime());
+        assertNotNull(droneLoadResponse.getEndTime());
+        droneLoadResponse.setStartTime(mocks.DRONE_LOAD_START_TIME);
+        droneLoadResponse.setEndTime(mocks.DRONE_LOAD_END_TIME);
+        assertEquals(
+                droneLoadResponse,
+                mocks.baseDroneLoadResponse(droneId, IDLE, droneLoadMedicationResponses, mocks.DRONE_LOAD_END_TIME)
         );
     }
 }
